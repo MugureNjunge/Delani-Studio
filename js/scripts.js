4f0a19b85402ae1f751de18f7f2a7583c5d4611e
MugureNjunge <maureennjunge22@gmail.com>
2022-03-11 19:51:38 +0300

    style using bootstrap and css

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function userData() {
+  let name = document.getElementById(name).value;
+  let email = document.getElementById(email).value;
+  let message = document.getElementById(message).value;
+
+
+  if userData === (name && email && message && submit){
+    alert("name" + , we have received your message.Thank you for contacting us!)
+  } else {
+    alert("All fields must be filled")
+  }
+
+}  
+
+
