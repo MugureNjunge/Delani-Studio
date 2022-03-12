c58a873dc1177069018a1ae8c127d76d8362a655
MugureNjunge <maureennjunge22@gmail.com>
2022-03-12 14:30:03 +0300

    add text and style to the 'what we do' part

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 function userData() {
-  let name = document.getElementById(name).value;
-  let email = document.getElementById(email).value;
-  let message = document.getElementById(message).value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
 
 
-  if (name.length === 0){
-    alert("Hi" + name +  "we have received your message.Thank you for contacting us!")
-  } else {
-    alert("All fields must be filled")
+  if (name.length === 0 || email.length === 0 || message.length === 0){
+    alert("All fields must be filled");
+    return false;
   }
-
+  else {
+    alert("Hi " + name + ", we have received your message.Thank you for contacting us!");
+  }
+  
 }  
 
